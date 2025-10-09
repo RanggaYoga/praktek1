6a55ef034cf33db0551e5517ef2141f5d27b9528
RanggaYoga <lawe8996@gmail.com>
2025-10-09 09:18:05 +0700

    Push3

--- data_kelurahan/warga/views.py
+++ data_kelurahan/warga/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Warga, Pengaduan
+from .forms import WargaForm, PengaduanForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -10,3 +12,13 @@ class WargaDetailView(DetailView):
     model = Warga
 class PengaduanListView(ListView):
     model = Pengaduan 
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
